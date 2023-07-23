6edef702ef8b24397af23d0c855a65d10508ca41
Nahid4306053 <131442604+Nahid4306053@users.noreply.github.com>
2023-07-24 00:56:19 +0600

    Nahid-Chatbot

--- main.js
+++ main.js
@@ -195,3 +195,19 @@ fetch('https://free.churchless.tech/v1/chat/completions', {
 
    
 })
+
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
